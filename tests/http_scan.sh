4040c4fd7c21a6766fe86af8668aca0481099e3c
CourtneyJane2904 <34748111+CourtneyJane2904@users.noreply.github.com>
2023-04-20 00:19:33 +0100

    Update http_scan.sh

--- tests/http_scan.sh
+++ tests/http_scan.sh
@@ -17,6 +17,6 @@ nc -v "${host}" "${port}" > "test_results/http/${host}/banner_p${port}"
 nmap -p${port} $host -sCV -oA "test_results/http/${host}/general_p${port}" &
 whatweb -a 3 "http://${host}:${port}" > "test_results/http/${host}/whatweb_enum_p${port}" &
 nikto -h "${ip}:${i}" -nointeractive -maxtime 360 >> "test_results/http/${host}/nikto_enum_p${port}" &
-timeout 360 gobuster dir -r -u "http://${host}:${port}/" -w /usr/share/wordlists/dirb/common.txt -t 40 -x .js,.js.map,.txt > "test_results/http/${host}/pages_found_p${port}" &
+timeout 360 gobuster dir -r -u "http://${host}:${port}/" -w wordlists/small_dir_list.txt -t 40 -x .js,.js.map,.txt > "test_results/http/${host}/pages_found_p${port}" &
 echo "HTTP scans on ${host}:${port} launched."
 exit 0
