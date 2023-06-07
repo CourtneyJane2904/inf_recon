d99247781752821093885f61aa1caa332c3ca000
CourtneyJane2904 <courtneyevans2904@gmail.com>
2023-06-07 17:51:59 +0100

    Bug fixes

--- tests/microsoft-ds_scan.sh
+++ tests/microsoft-ds_scan.sh
@@ -16,6 +16,6 @@ echo "Launching SMB scans on ${host}:${port}"
 # run default nmap scripts for ftp and retrieve version
 nmap -p${port} $host -sCV -oA "${dest_dir}/general_p${port}" &
 nmap --script "safe or smb-enum-* or smb-vuln*" -p "${port}" "${host}" -oA "${dest_dir}/nmap_enum_p${port}" &
-timeout 300s enum4linux -a "${ip}" > "${dest_dir}/enum4linux_p${port}" &
+timeout 300s enum4linux -a "${host}" > "${dest_dir}/enum4linux_p${port}" &
 echo "SMB scans on ${host}:${port} launched."
 exit 0
