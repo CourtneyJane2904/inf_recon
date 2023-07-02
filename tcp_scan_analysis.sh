c7fac773709c4f344a820a83f6b747d68f6e4182
CourtneyJane2904 <courtneyevans2904@gmail.com>
2023-07-02 17:25:13 +0100

    Remove arrays from scan_analysis files.

--- tcp_scan_analysis.sh
+++ tcp_scan_analysis.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
 filename=$1
-# create analysis dir for storage of data generated from this script
-# create files of hosts by services, useful for performing service-specific scans
-declare -a tcp_svcs=( \
-	" ssh" " ftp$" " smtp" " telnet" \
-	" http$" " https" " isakmp" " microsoft-ds" \
-	" http-proxy" " ftps" " pop2$" " pop3$" " pop3s" " pop2s" \
-	" netbios-ssn" " ms-wbt-server" " wsman" " ms-cluster-net" \
-	" winrm" " msrpc" " nfs" " nrpe" " ident" \
-	" smb" " printer" " jetdirect" " svrloc" " llmnr" \
-	" globalcatLDAP$" " globalcatLDAPssl" " ldap$" " ldaps" " ssdp" \
-	" upnp" " http-rpc-epmap" " domain" " sftp" \
-	" kerberos-sec" " login" " shell$" \
-	" exec$" " mysql" " ndmp" " imaps$" " iscsi" \
-	" oracle-tns" " ms-sql" " rtsp" " imap$" \
-	" finger" " tacacs" " vnc" \
-	)
 
 for host_file in $(find analysis/tcp_host_lists_by_svc -name *_hosts.txt); do
 	svc=$(echo ${host_file} | cut -d '/' -f3 | cut -d '_' -f1)
--- udp_scan_analysis.sh
+++ udp_scan_analysis.sh
@@ -2,20 +2,6 @@
 
 filename=$1
 # create files of hosts by services, useful for performing service-specific scans
-declare -a tcp_svcs=( \
-	" ssh" " ftp$" " smtp" " telnet" \
-	" http$" " https" " isakmp" " microsoft-ds" \
-	" http-proxy" " ftps" " pop2$" " pop3$" " pop3s" " pop2s" \
-	" netbios-ssn" " ms-wbt-server" " wsman" " ms-cluster-net" \
-	" winrm" " msrpc" " nfs" " nrpe" " ident" \
-	" smb" " printer" " jetdirect" " svrloc" " llmnr" \
-	" globalcatLDAP$" " globalcatLDAPssl" " ldap$" " ldaps" " ssdp" \
-	" upnp" " http-rpc-epmap" " domain" " sftp" \
-	" kerberos-sec" " login" " shell$" \
-	" exec$" " mysql" " ndmp" " imaps$" " iscsi" \
-	" oracle-tns" " ms-sql" " rtsp" " imap$" \
-	" finger" " tacacs" " vnc" \
-	)
 
 for host_file in $(find analysis/udp_host_lists_by_svc -name *_hosts.txt); do
 	svc=$(echo ${host_file} | cut -d '/' -f3 | cut -d '_' -f1)
@@ -29,6 +15,6 @@ for host_file in $(find analysis/udp_host_lists_by_svc -name *_hosts.txt); do
 		"tests/${svc}_scan.sh" "${ip}" "${port}" &
 	done < "${host_file}"
 done
-echo "TCP analysis complete."
+echo "UDP analysis complete."
 stty echo
 exit 0
