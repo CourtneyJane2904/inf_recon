a2ac135cc69143042730ab10715ac8dfb55b4a14
CourtneyJane2904 <34748111+CourtneyJane2904@users.noreply.github.com>
2023-07-23 20:01:08 +0100

    Update tftp_scan.sh

--- tests/tftp_scan.sh
+++ tests/tftp_scan.sh
@@ -3,7 +3,7 @@
 # https://book.hacktricks.xyz/network-services-pentesting/69-udp-tftp
 host=$1
 port=69
-desr_dir="svc_scan_results/${host}/tftp"
+dest_dir="svc_scan_results/${host}/tftp"
 mkdir -p "${dest_dir}"
 
 if [[ ! -z "${2}" ]]; then
@@ -13,6 +13,6 @@ fi
 echo "Launching TFTP scans on ${host}:${port}"
 
 nmap -Pn -sU -p"${port}" -sV --script tftp-enum "${host}" -oA "${dest_dir}/tftp_enum_p${port}" &
-
+`msfconsole -q -x "spool ${dest_dir}/tftp_brute_msf_p${port};use auxiliary/scanner/tftp/tftpbrute;set RHOSTS ${host};exploit;exit"
 echo "TFTP scans on ${host}:${port} launched."
 exit 0
