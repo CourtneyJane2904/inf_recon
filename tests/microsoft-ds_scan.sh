4018c56010e3afacac9b8cccd80b53f233c1c25e
CourtneyJane2904 <34748111+CourtneyJane2904@users.noreply.github.com>
2023-07-25 21:51:54 +0100

    Update microsoft-ds_scan.sh

--- tests/microsoft-ds_scan.sh
+++ tests/microsoft-ds_scan.sh
@@ -5,6 +5,7 @@ host=$1
 port=445
 dest_dir="svc_scan_results/${host}/microsoft-ds"
 mkdir -p "${dest_dir}"
+ssh_creds="wordlists/ssh_default_creds.txt"
 
 if [[ ! -z "${2}" ]]; then
 	port=$2
@@ -18,5 +19,6 @@ nmap -Pn -p${port} $host -sCV -oA "${dest_dir}/general_p${port}" &
 nmap -Pn --script "safe or smb-enum-* or smb-vuln*" -p "${port}" "${host}" -oA "${dest_dir}/nmap_enum_p${port}" &
 nmap -Pn --script smb-enum-users -p "${port}" "${host}" -oA "${dest_dir}/nmap_users_enum_p${port}" &
 timeout 300s enum4linux -a "${host}" > "${dest_dir}/enum4linux_p${port}" &
+hydra -t 1 -V -f -o "${dest_dir}/hydra_p${port}" -C ${ssh_creds} $ip smb &
 echo "SMB scans on ${host}:${port} launched."
 exit 0
