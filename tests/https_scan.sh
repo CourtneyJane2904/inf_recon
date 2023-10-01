6903d6eb38c32a27e7b789f7e894dfc916c144bc
CourtneyJane2904 <34748111+CourtneyJane2904@users.noreply.github.com>
2023-10-01 17:27:44 +0100

    Update https_scan.sh
    Correction to gobuster command.

--- tests/https_scan.sh
+++ tests/https_scan.sh
@@ -20,6 +20,6 @@ nmap -Pn -p${port} $host -sCV -oA "${dest_dir}/general_p${port}" &
 "${testssl}" "https://${host}:${port}" > "${dest_dir}/ssl_issues_p${port}" &
 whatweb -a 3 "https://${host}:${port}" > "${dest_dir}/whatweb_enum_p${port}" &
 nikto -h "${host}:${port}" -nointeractive -maxtime 360 >> "${dest_dir}/nikto_enum_p${port}" &
-timeout 360 gobuster dir -r -u "https://${host}:${port}/" -w wordlists/small_dir_list.txt -t 40 -x .js,.js.map,.txt > "${dest_dir}/pages_found_p${port}" &
+timeout 360 gobuster -r -u "https://${host}:${port}/" -w wordlists/small_dir_list.txt -t 40 -x .js,.js.map,.txt > "${dest_dir}/pages_found_p${port}" &
 echo "HTTPS scans on ${host}:${port} launched."
 exit 0
